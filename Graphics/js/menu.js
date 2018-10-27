305d001a3ef1a88b962fbf47db1a70e7a1aa0536
mtaleksa1 <mtaleksa1@e8cbbb29-0188-4d8e-a734-5b1006470297>
2018-10-27 00:17:03 +0000

    add basic volume on/off button to menu - needs logic to implement volume control
    git-svn-id: https://svn.riouxsvn.com/invadergame@13 e8cbbb29-0188-4d8e-a734-5b1006470297

--- Graphics/js/menu.js
+++ Graphics/js/menu.js
@@ -1,5 +1,12 @@
 var menuState = {
 
+    preload : function () {
+
+        game.load.image('volumeOn', '/assets/sprites/volume_on.png');
+        game.load.image('volumeOff', '/assets/sprites/volume_off.png');
+
+    },
+
     create : function () {
         console.log("DEBUG: in menu state");
 
@@ -11,7 +18,6 @@ var menuState = {
             boundsAlignH: "center"
         }).setTextBounds(1, 1);
 
-
         // instructions to start game
         // setTextBounds allows text to be centered on canvas
         game.add.text(WIDTH/2, HEIGHT/1.5, 'PRESS ANY KEY TO START', {
@@ -19,6 +25,13 @@ var menuState = {
             fill: "White",
             boundsAlignH: "center"
         }).setTextBounds(1, 1);
+
+        // mute button for global volume - sets global 'volume' variable declared in game.js to either 1 or 0
+        muteButton = game.add.image(WIDTH/2, HEIGHT/1.3, 'volumeOn');
+        muteButton.anchor.setTo(0.5, 0.5);
+        muteButton.scale.x = .5;
+        muteButton.scale.y = .5;
+
     },
 
     update : function () {

9e3a777655acb460c2e37a90594601f5e9069480
cdngouma <cdngouma@e8cbbb29-0188-4d8e-a734-5b1006470297>
2018-10-30 05:38:24 +0000

    added player movement controls + firing
    git-svn-id: https://svn.riouxsvn.com/invadergame@29 e8cbbb29-0188-4d8e-a734-5b1006470297

--- src/js/play.js
+++ src/js/play.js
@@ -1,9 +1,15 @@
+var bulletTime = 0;
+
 var playState = {
 
     preload : function () {
         // Load game assets
-        game.load.audio('explosion1', '/assets/soundfx/zapsplat_explosion_1.mp3');
-        game.load.audio('explosion2', '/assets/soundfx/zapsplat_explosion_2.mp3');
+        game.load.audio('explosion1', 'assets/soundfx/zapsplat_explosion_1.mp3');
+        game.load.audio('explosion2', 'assets/soundfx/zapsplat_explosion_2.mp3');
+        game.load.image('player', 'assets/sprites/player.png');
+        game.load.image('bullet0', 'assets/sprites/bullet0.png');
+        game.load.image('bullet1', 'assets/sprites/bullet1.png');
+        game.load.image('bullet2', 'assets/sprites/bullet3.png');
         score = new Score(0);
         lives = new Lives(3);
         level = 1;
@@ -16,10 +22,32 @@ var playState = {
         explosion1 = new Phaser.Sound(game, 'explosion1', volume, false);
         explosion2 = new Phaser.Sound(game, 'explosion2', volume, false);
 
+        // create player
+        player = game.add.sprite(32, 32, 'player');
+        player.x = WIDTH / 2;
+        player.y = HEIGHT - 20;
+        player.anchor.setTo(0.5, 0.5);
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+
+        //  create a bullet group for player
+        bullets = game.add.group();
+        bullets.enableBody = true;
+        bullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+        var numBullets = 30;    // N number of bullets needed for N enemies
+        // adding bullets to group
+        for (var i = 0; i < numBullets; i++){
+            // randomly pick a image for each bullet
+            var b = bullets.create(0, 0, 'bullet' + Math.floor(Math.random() * 100) % 3);
+            b.anchor.setTo(0.5, 1.5);
+            b.checkWorldBounds = true;
+            b.outOfBoundsKill = true;
+        }
+
         // create controls
-        //      these are currently just set up to test sounds
+        // these are currently just set up to test sounds
         cursors = game.input.keyboard.createCursorKeys();
-        fireButton = game.input.keyboard.addKey(Phaser.Keyboard.CONTROL);
+        fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
         // Display the score
         scoreDisplay = game.add.text(WIDTH-30, 20, score.getScore(), {
@@ -48,13 +76,35 @@ var playState = {
     },
 
     update : function ()  {
+        var playerSpeed = 200;
+        // MOVE PLAYER AROUND
+        if(player.alive){
+            // stop player then check for movement keys
+            player.body.velocity.setTo(0, 0);
+
+            if(cursors.left.isDown){
+                player.body.velocity.x = -playerSpeed;
+            } else if(cursors.right.isDown){
+                player.body.velocity.x = playerSpeed;
+            }
+
+            // if player is out of bound, it appers on the other sides
+            if(player.x < 0) player.x = WIDTH;
+            else if(player.x > WIDTH) player.x = 0;
+
+            if(fireButton.isDown){
+                fireBullet();
+            }
+        }
 
         // SOUND TEST
-        if (cursors.left.isDown) {
+        soundBtn1 = game.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_1);
+        soundBtn2 = game.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_0);
+        if (soundBtn1.isDown) {
             cursors.left.isDown = false;
             explosion1.play();
         }
-        if (cursors.right.isDown) {
+        if (soundBtn2.isDown) {
             cursors.right.isDown = false;
             explosion2.play();
         }
@@ -70,3 +120,19 @@ var playState = {
     },
 
 };
+
+function fireBullet(){
+    var bulletSpeed = 400;
+
+    if(game.time.now > bulletTime){
+        // create a bullet then
+        bullet = bullets.getFirstExists(false);
+
+        if (bullet){
+            // fire it
+            bullet.reset(player.x, player.y + 8);
+            bullet.body.velocity.y = -bulletSpeed;
+            bulletTime = game.time.now + 200;
+        }
+    }
+}
